388f6510576d4c01d82f97e545a24bcd22d9c09e
soryual <sara@pixeldirects.com>
2013-06-23 15:45:15 -0700

    Initial Commit

---
+++ educake/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Educake::Application.config.secret_token = '73a5b0c3fd49b78cb44e0ac2403bf4e4a5a3ec8545c9b4edbe2ef750f2e629210df6ba88be4e327d63b2cc4ee034e665f32cb08ef68ef27d7438d3497d04bdce'
